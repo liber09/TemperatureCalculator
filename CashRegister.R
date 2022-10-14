3737db7872015866ae1161e8c85b0af947503340
liber09 <113246195+liber09@users.noreply.github.com>
2022-10-14 05:39:27 +0200

    Cash registry now working

--- CashRegister.R
+++ CashRegister.R
@@ -1,14 +1,30 @@
-products
-count
+products <- c()
+count <- c()
+sum <- 0
+amountInRegistry <- 2000
+productPrices <- c(5,2,2,3,3)
 showProducts <- function(){
-  cat("Products you can buy\n1.Bread\n2.Butter\n3.Cheese\n4.Ham\n5.Eggs\n")
+  cat("Products you can buy\n1.Bread 5$\n2.Butter 2$\n3.Cheese 2$\n4.Ham 3$\n5.Eggs 3$\n")
 }
 
+recordSaleValue <- function(){
+  for(i in 1:length(products)){
+    sum <- sum + (count[i]*productPrices[products[i]])
+  }
+  return(sum)
+}
+giveOutChange <- function(sumToPay, paymentRecieved){
+  amountInRegistry <<- amountInRegistry + paymentRecieved
+  change <- paymentRecieved-sumToPay 
+  amountInRegistry <<- amountInRegistry - change
+  return(change)
+}
 
+checkBalance <- function(){
+  moneyInCashRegistry <- amountInRegistry
+  return(moneyInCashRegistry)
+}
 
-#recordSaleValue() <- function
-#giveOutChange()
-#checkBalance()
 
 showProducts()
 wantToAddMore <- "y"
@@ -17,3 +33,13 @@ while(wantToAddMore == "y"){
    count[length(count)+1] <- as.integer(c(readline(prompt = "How many would you like to buy?")))
   wantToAddMore <- readline(prompt = "Do you want to buy anything else?")
 }
+sumToPay <- recordSaleValue()
+cat("Your amount to pay is:")
+cat(sumToPay)
+paymentRecieved <- as.integer(readline(prompt = "How much do you pay? "))
+moneyBack <- giveOutChange(sumToPay,paymentRecieved)
+cat("You get money back: ")
+cat(moneyBack)
+cat("\nMoney in cashregistry is now: ")
+cashInRegistry <- checkBalance()
+cat(cashInRegistry)
